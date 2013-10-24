7104d880d1513481597781ce4165a7250d53399e
colincove <cove.colin@gmail.com>
2013-10-24 10:15:40 -0700

    base functionality of webservices has been implemented. No data storage, only login from email so far

--- workspace/CreativeCritic/src/com/example/creativecritic/MainActivity.java
+++ workspace/CreativeCritic/src/com/example/creativecritic/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.creativecritic;
 
+import com.example.creativecritic.webservices.IResultListener;
+import com.example.creativecritic.webservices.Result;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements IResultListener {
+	private CreateCriticWebservices webservices;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		webservices = CreateCriticWebservices.getInstance();
+		webservices.requestUserFromEmail(this);
+		
 	}
 
 	@Override
@@ -17,6 +23,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void result(Result result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resultFail() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ workspace/CreativeCritic/src/com/example/creativecritic/webservices/IResultListener.java
@@ -0,0 +1,6 @@
+package com.example.creativecritic.webservices;
+
+public interface IResultListener {
+	public void result(Result result);
+	public void resultFail();
+}
